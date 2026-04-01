e6b9d31c18285c0513af421361edbaa50f8da0f1
Sjaize <sjaiz1717@gmail.com>
2026-04-02 01:47:19 +0900

    chore: Railway 배포 설정 추가

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ def health_check():
 
 
 if __name__ == "__main__":
+    import os
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
